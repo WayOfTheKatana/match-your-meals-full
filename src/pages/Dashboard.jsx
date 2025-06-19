22927679c1a48d7c01e4e811b7838fcd06f87657
Shajid Shafee <shajid1biz@gmail.com>
2025-06-19 13:15:28 +0530

    Updated Dashboard.jsx

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -64,7 +64,7 @@ const Dashboard = () => {
                 Creator Mode
               </Button>
 
-              <div className="flex items-center space-x-2">
+              <div className="flex items-center space-x-4">
                 <div className="flex items-center space-x-2">
                 <div className="w-8 h-8 bg-primary-600 rounded-full flex items-center justify-center">
                   <User className="w-4 h-4 text-white" />
