89dc543754d042373dabd44e92be33a8aa7ef9a8
Shajid Shafee <shajid1biz@gmail.com>
2025-06-29 20:21:07 +0530

    Updated Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -78,7 +78,8 @@ const Home = () => {
       <div className="py-20 bg-white text-center">
         <div className="max-w-4xl mx-auto px-4">
           <SplitText
-            text="Stop scrolling through endless life stories just to find a recipe"
+            text="Stop scrolling through endless life <br /> stories just to find a recipe"
+            text="Hello World"
             className="text-3xl md:text-5xl font-semibold text-gray-900 mb-8"
             delay={100}
             duration={2}
