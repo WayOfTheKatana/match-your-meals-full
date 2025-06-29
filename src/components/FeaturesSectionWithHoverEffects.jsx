be439ae3ae98be14c711c1c76f2e8d2923224d04
Shajid Shafee <shajid1biz@gmail.com>
2025-06-30 00:53:52 +0530

    Updated FeaturesSectionWithHoverEffects.jsx

--- src/components/FeaturesSectionWithHoverEffects.jsx
+++ src/components/FeaturesSectionWithHoverEffects.jsx
@@ -31,9 +31,9 @@ Just type "quick breakfast for kids" or "low-carb dinner" and get exactly what y
 ,
     },
     {
-      title: "Voice Commands",
+      title: "Voice Search",
       description:
-        "Speak your recipe requests naturally and get instant results, perfect for hands-free cooking.",
+        `Too busy to type? Just say "find me a 20-minute pasta recipe" and we'll handle the rest.`,
       icon: <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-6">
   <path d="M8.25 4.5a3.75 3.75 0 1 1 7.5 0v8.25a3.75 3.75 0 1 1-7.5 0V4.5Z" />
   <path d="M6 10.5a.75.75 0 0 1 .75.75v1.5a5.25 5.25 0 1 0 10.5 0v-1.5a.75.75 0 0 1 1.5 0v1.5a6.751 6.751 0 0 1-6 6.709v2.291h3a.75.75 0 0 1 0 1.5h-7.5a.75.75 0 0 1 0-1.5h3v-2.291a6.751 6.751 0 0 1-6-6.709v-1.5A.75.75 0 0 1 6 10.5Z" />
