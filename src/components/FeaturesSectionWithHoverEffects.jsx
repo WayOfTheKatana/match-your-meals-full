867cd11a8f9cd164dadb13391e7f8f8c27c4a072
Shajid Shafee <shajid1biz@gmail.com>
2025-06-30 00:35:21 +0530

    Update Feature Section Colors

--- src/components/FeaturesSectionWithHoverEffects.jsx
+++ src/components/FeaturesSectionWithHoverEffects.jsx
@@ -88,21 +88,21 @@ const Feature = ({
       )}
     >
       {index < 4 && (
-        <div className="opacity-0 group-hover/feature:opacity-100 transition duration-200 absolute inset-0 h-full w-full bg-gradient-to-t from-gray-100 dark:from-gray-800 to-transparent pointer-events-none" />
+        <div className="opacity-0 group-hover/feature:opacity-100 transition duration-200 absolute inset-0 h-full w-full bg-gradient-to-t from-primary-50 dark:from-gray-800 to-transparent pointer-events-none" />
       )}
       {index >= 4 && (
-        <div className="opacity-0 group-hover/feature:opacity-100 transition duration-200 absolute inset-0 h-full w-full bg-gradient-to-b from-gray-100 dark:from-gray-800 to-transparent pointer-events-none" />
+        <div className="opacity-0 group-hover/feature:opacity-100 transition duration-200 absolute inset-0 h-full w-full bg-gradient-to-b from-primary-50 dark:from-gray-800 to-transparent pointer-events-none" />
       )}
-      <div className="mb-4 relative z-10 px-10 text-gray-600 dark:text-gray-400">
+      <div className="mb-4 relative z-10 px-10 text-gray-900 dark:text-gray-400">
         {React.cloneElement(icon, { className: "w-6 h-6 text-primary-600" })}
       </div>
       <div className="text-lg font-bold mb-2 relative z-10 px-10">
         <div className="absolute left-0 inset-y-0 h-6 group-hover/feature:h-8 w-1 rounded-tr-full rounded-br-full bg-gray-300 dark:bg-gray-700 group-hover/feature:bg-primary-600 transition-all duration-200 origin-center" />
-        <span className="group-hover/feature:translate-x-2 transition duration-200 inline-block text-gray-800 dark:text-gray-100">
+        <span className="group-hover/feature:translate-x-2 transition duration-200 inline-block text-gray-950 dark:text-gray-100">
           {title}
         </span>
       </div>
-      <p className="text-sm text-gray-600 dark:text-gray-300 max-w-xs relative z-10 px-10">
+      <p className="text-sm text-gray-900 dark:text-gray-300 max-w-xs relative z-10 px-10">
         {description}
       </p>
     </div>
