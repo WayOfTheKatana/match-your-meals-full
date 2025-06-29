d7a1c9e83254a37e94c036d0875efa4fdf62c62b
Shajid Shafee <shajid1biz@gmail.com>
2025-06-30 00:41:19 +0530

    Updated FeaturesSectionWithHoverEffects.jsx

--- src/components/FeaturesSectionWithHoverEffects.jsx
+++ src/components/FeaturesSectionWithHoverEffects.jsx
@@ -93,11 +93,11 @@ const Feature = ({
       {index >= 4 && (
         <div className="opacity-0 group-hover/feature:opacity-100 transition duration-200 absolute inset-0 h-full w-full bg-gradient-to-b from-primary-50 to-transparent pointer-events-none" />
       )}
-      <div className="mb-4 relative z-10 px-10 text-gray-200">
+      <div className="mb-4 relative z-10 px-10 text-gray-900">
         {React.cloneElement(icon, { className: "w-6 h-6 text-primary-600" })}
       </div>
       <div className="text-lg font-bold mb-2 relative z-10 px-10">
-        <div className="absolute left-0 inset-y-0 h-6 group-hover/feature:h-8 w-1 rounded-tr-full rounded-br-full bg-gray-300 dark:bg-gray-700 group-hover/feature:bg-primary-600 transition-all duration-200 origin-center" />
+        <div className="absolute left-0 inset-y-0 h-6 group-hover/feature:h-8 w-1 rounded-tr-full rounded-br-full bg-gray-300 group-hover/feature:bg-primary-600 transition-all duration-200 origin-center" />
         <span className="group-hover/feature:translate-x-2 transition duration-200 inline-block text-gray-950">
           {title}
         </span>
