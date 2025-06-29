74a1108600ee08e35d3d9688f3a4417e3831236d
Shajid Shafee <shajid1biz@gmail.com>
2025-06-30 03:59:51 +0530

    Updated CommonHeader.jsx

--- src/components/CommonHeader.jsx
+++ src/components/CommonHeader.jsx
@@ -97,7 +97,6 @@ export const CommonHeader = ({ variant = 'default' }) => {
                       
                       <button
                         onClick={() => {
-                           e.stopPropagation();
                           navigate('/dashboard');
                           setShowUserMenu(false);
                         }}
