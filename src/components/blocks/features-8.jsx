e1418a5bc8cd7dcecd2a69c6478bc088b1141e44
Shajid Shafee <shajid1biz@gmail.com>
2025-06-29 20:51:26 +0530

    Updated features-8.jsx

--- src/components/blocks/features-8.jsx
+++ src/components/blocks/features-8.jsx
@@ -18,7 +18,7 @@ export function Features() {
                                     </svg>
                                     <span className="mx-auto block w-fit text-5xl font-semibold">100%</span>
                                 </div>
-                                <h2 className="mt-6 text-center text-3xl font-semibold">Customizable</h2>
+                                <h2 className="mt-6 text-center text-3xl font-semibold">Authentic Recipe</h2>
                             </CardContent>
                         </Card>
                         <Card className="relative col-span-full overflow-hidden sm:col-span-3 lg:col-span-2">
